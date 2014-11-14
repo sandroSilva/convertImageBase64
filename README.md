1dac95a613b9265bf0695d07a8a59a7763d70d7f
Sandro Silva <sandrilho@gmail.com>
2014-11-14 12:30:55 -0200

    arquivos comentados
    arquivos comentados

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ Convertendo arquivos de imagens para arquivos base/64
 ============
 
 Conversão de Arquivo de imagem em base/64
+
+para a conversão de imagens de qualquer tipo para arquivos de base 64 eu usei algumas bibliotecas em CDN:
+
+jQuery
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+Bootstrap
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+E um plugin para gerar uma imagem de holder chamado Holder.js.
+http://imsky.github.io/holder/
+
+
+O que faço é mascarar o input de file por um botão de upload que vai ativar input de file normalmente.
+Após isto ocorre a conversão da imagem do computador para um arquivo base64 que poderá ser salvo ou cadastrado em um banco de dados.
--- js/main.js
+++ js/main.js
@@ -1,32 +1,39 @@
 var fileUploadSend;
 var dataImgHolder;
+/* esconde os botões de action da imagem */
 $(".buttonsActionImg").hide();
 
 function sendFile(){
-
 	if ( document.getElementById("inputSendImage").files && document.getElementById("inputSendImage").files[0] ) {
+		/* prepara a função de leitura da imagem e conversão FileReader() na varável FR */
 		var FR= new FileReader();
+		/* recebe os dados de conversão da imagem com os dados do input escondido */ 
 		FR.onload = function(e) {
+			/* salva a imagem de holder para normalizar, quando cancelado. */
 			dataImgHolder = $("#docImgSend").attr('src');
+			/* coloca na variável fileUploadSend o valor em base/64 */
 			fileUploadSend = e.target.result;
 			$("#docImgSend").replaceWith('<img src="'+e.target.result+'" id="docImgSend" />');
 			$(".buttonsActionImg").show();
-        };       
+        };
+        /* inicia a função de leitura da imagem e conversão da imagem com os dados do input escondido */    
         FR.readAsDataURL( document.getElementById("inputSendImage").files[0] );
     }
 }
 
-
+/* cancela e volta ao estado normal da imagem para */
 function fCancelUpload(){
 	$("#docImgSend").replaceWith('<img id="docImgSend" src="'+dataImgHolder+'" alt="...">');
 	$(".buttonsActionImg").hide();
 }
 
+/* exibe através da saída console a codificação do arquivo base/64 */
 function fConfirmUpload(){
 	console.log(fileUploadSend);
 }
 
 
+/* captura os estados dos botões */
 $( "#btSendImage" ).change(function() {
 	sendFile();
 });
